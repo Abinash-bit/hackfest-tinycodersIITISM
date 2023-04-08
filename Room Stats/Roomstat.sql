a2e430c49feab3c3e7e52e7ecc8113930b861700
Abinash Mahapatra <89697294+Abinash-bit@users.noreply.github.com>
2023-04-08 10:54:47 +0530

    Delete Roomstat.sql
    Signed-off-by: Abinash Mahapatra <89697294+Abinash-bit@users.noreply.github.com>

--- Room Stats/Roomstat.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE statistics (
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	total_persons INT(6) NOT NULL,
-	persons_entered INT(6) NOT NULL,
-	persons_left INT(6) NOT NULL,
-	food_consumed INT(6) NOT NULL,
-	food_left INT(6) NOT NULL,
-	submission_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
